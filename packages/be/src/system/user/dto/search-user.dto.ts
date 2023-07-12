8bb8f72f8918323a07bc693f698bead02269e405
wanghaitao <15124505701@163.com>
2023-07-12 10:21:13 +0800

    fix: search user by email

--- packages/be/src/system/user/dto/search-user.dto.ts
+++ packages/be/src/system/user/dto/search-user.dto.ts
@@ -1,9 +1,16 @@
 import { IntersectionType, PartialType, PickType } from '@nestjs/swagger';
 import { User } from '../entities/user.entity';
 import { PageParams } from '@/common/api/common-page';
+import { Allow } from 'class-validator';
 
 export class SearchUserDtoWithNotPage extends PartialType(
-  PickType(User, ['username', 'email', 'nickName', 'status', 'positionIds', 'departmentIds', 'roleIds']),
-) {}
+  PickType(User, ['username', 'nickName', 'status', 'positionIds', 'departmentIds', 'roleIds']),
+) {
+  /**
+   * 邮箱
+   */
+  @Allow()
+  email?: string;
+}
 
 export class SearchUserDto extends IntersectionType(PageParams, SearchUserDtoWithNotPage) {}
